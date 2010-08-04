c46a1a6083d5a38364476bb4b819053744d5472d
Theresa Calderon <cgtheresa@gmail.com>
2010-08-04 15:01:59 -0700

    update parameter descriptions controller

--- app/controllers/parameter_descriptions_controller.rb
+++ app/controllers/parameter_descriptions_controller.rb
@@ -1,5 +1,8 @@
 class ParameterDescriptionsController < ApplicationController
   def index
     @parameters = ParameterDescriptions.find(:all,:order => ["Parameter"])
+    @primary = ParameterGroup.find(:first, :conditions => "`group` = 'CCHDO Primary Parameters'").parameters
+    @secondary = ParameterGroup.find(:first, :conditions => "`group` = 'CCHDO Secondary Parameters'").parameters
+    @tertiary = ParameterGroup.find(:first, :conditions => "`group` = 'CCHDO Tertiary Parameters'").parameters
   end
 end
