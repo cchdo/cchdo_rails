9933a42a4613e5234028bdb1437d81ea6b9f0d11
Justin Fields <jfields@Justin-Fieldss-MacBook-Pro.local>
2010-09-17 13:17:34 -0400

    I've added a new data history view for seeing all histories at once. I've edited the controller and main.css a bit to support this.

--- app/controllers/data_history_controller.rb
+++ app/controllers/data_history_controller.rb
@@ -34,4 +34,28 @@ class DataHistoryController < ApplicationController
          @note_entry[:Note].gsub!(/[\t]/,"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
       end      
    end
+   
+   def full
+     @expo=params[:ExpoCode]
+     @note = params[:Note]
+     @cur_sort = params[:Sort]
+     @notes = []
+     if(@expo)
+        @updated = Event.find_by_sql("SELECT * FROM events ORDER BY Date_Entered DESC LIMIT 1")
+         #@updated = Event.find(:first,:order=>'Date_Entered DESC')
+         if (@expo)
+            if(@cur_sort == "LastName")
+               @events = Event.find(:all,:conditions=>["ExpoCode='#{@expo}'"],:order=>['LastName'])
+            elsif( @cur_sort == "Data_Type")
+               @events = Event.find(:all,:conditions=>["ExpoCode='#{@expo}'"],:order=>['Data_Type'])
+            else
+               @events = Event.find(:all,:conditions=>["ExpoCode='#{@expo}'"],:order=>['Date_Entered DESC'])
+            end
+            @cruise = Cruise.find(:first,:conditions=>["ExpoCode='#{@expo}'"])
+         end
+     else
+        redirect_to "http://cchdo.ucsd.edu/data_access"
+     end
+   end
+   
 end
