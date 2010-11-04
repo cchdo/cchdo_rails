1d22d1fbf0c3ac24bf40f17a51b7a597fe03c70b
Matt Shen <myeeshen@gmail.com>
2010-11-04 15:33:46 -0700

    Update static controller to redirect 404s properly and remove ocean static pages.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,44 +1,43 @@
 class StaticController < ApplicationController
-    NO_CACHE = [
-      'static/about/website',
-      'index',
-  ]
-  
+
   def index
-     if template_exists? path = 'static/' + params[:path].join('/')
-        render_cached path
-     else
-       if template_exists? path += 'index.rhtml'
-         render_cached path
-       else
-          redirect_to :path => '404.html'
-          #raise ::ActionController::RoutingError,
-          #      "Recognition failed for #{request.path.inspect}"
-       end
-     end
+    basepath = params[:path] || ''
+    basepath = basepath.join('/') if basepath.kind_of?(Array)
+    basepath = 'static/' + basepath
+
+    render_cached(basepath) && return if template_exists?(basepath)
+    path = basepath + 'index.rhtml'
+    render_cached(path) && return if template_exists?(path)
+    path = basepath + 'index.html.erb'
+    render_cached(path) && return if template_exists?(path)
+
+    redirect_to '/404.html'
+    #raise ::ActionController::RoutingError, "Recognition failed for #{request.path.inspect}"
   end
-  
+
   private
 
-  # Define template_exists? for Rails 2.3 (cause it's deprecated)
-  unless ActionController::Base.private_instance_methods.include? 'template_exists?'
-    def template_exists?(path)
-      self.view_paths.find_template(path, response.template.template_format)
-    rescue ActionView::MissingTemplate
-      false
-    end
-  end
+    NO_CACHE = ['static/about/website']
 
-  def render_cached(path)
-    #if NO_CACHE.include? path
+    # It's deprecated in Rails 2.3
+    unless ActionController::Base.private_instance_methods.include? 'template_exists?'
+      def template_exists?(path)
+        self.view_paths.find_template(path, response.template.template_format)
+      rescue ActionView::MissingTemplate
+        false
+      end
+    end
+
+    def render_cached(path)
+      #if NO_CACHE.include? path
       render :template => path
-    #else
-    #   key = path.gsub('/', '-')
-    #   unless content = read_fragment(key)
-    #      content = render_to_string :template => path, :layout => false
-    #      write_fragment(key, content)
-    #   end
-    #   render :text => content, :layout => true
-    #end
-  end
+      #else
+      #   key = path.gsub('/', '-')
+      #   unless content = read_fragment(key)
+      #      content = render_to_string :template => path, :layout => false
+      #      write_fragment(key, content)
+      #   end
+      #   render :text => content, :layout => true
+      #end
+    end
 end
