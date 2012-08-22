807f16cc3a380e28fb5b2e1b6e56104aefbb73b1
Matt Shen <myeeshen@gmail.com>
2012-08-22 12:38:57 -0700

    Warn on bad file permissions in cruise_Status

--- app/helpers/cruise_status_helper.rb
+++ app/helpers/cruise_status_helper.rb
@@ -3,8 +3,11 @@ module CruiseStatusHelper
         output = ''
         if file_result
             output = link_to(link_text, file_result.FileName)
+            unless File.stat(file_result.FileName).readable?
+                output += content_tag(:span, 'File is not readable by public', :class => 'permissions')
+            end
             if file_result.respond_to?(:Stamp)
-                output += content_tag(:span, file_result.Stamp)
+                output += content_tag(:span, file_result.Stamp, :class => 'stamp')
             end
             output = content_tag(:li, output)
         end
