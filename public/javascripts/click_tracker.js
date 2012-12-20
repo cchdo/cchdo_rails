5b603011441c09a8bf45eb13934e02dd6a775d46
Matt Shen <myeeshen@gmail.com>
2012-12-20 11:59:09 -0800

    Hook in click tracker for cruise files

---
+++ public/javascripts/click_tracker.js
@@ -0,0 +1,21 @@
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+  return $('#data .box_content a').click(function(event) {
+    var expocode = $(this).attr('expocode');
+    var fileType = $(this).attr('file_type');
+    var string = '{"expocode":"' + expocode + '", "file_type":"' + fileType + '"}';
+    if (expocode) {
+      $.ajax({
+        url: 'http://hdo.ucsd.edu:60000/',
+        type: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        data: string,
+        async: false,
+        success: function(response) {console.log(response)},
+        error: function() {},
+        timeout: 500000
+      });
+    }
+  });
+});
