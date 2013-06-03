fc55813fea39af63298396a5bafb22c0047f1f96
Matt Shen <myeeshen@gmail.com>
2013-06-03 13:37:38 -0700

    Correct Indian basin code

--- app/controllers/by_ocean_controller.rb
+++ app/controllers/by_ocean_controller.rb
@@ -25,35 +25,55 @@ def southern
   @atlantic_basin = reduce_specifics(Cruise.find(:all, :conditions => ["`Group` LIKE ? AND (`Line` LIKE ? OR `Line` LIKE ? OR `Line` LIKE ? )", "%southern%", "A__", "AR%", "AJ%"], :order=>"Line, Begin_Date ASC"))
   @atlantic_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)}
 end 
-def indian 
-  @documents =[]
-  Document.find(:all, :select=>"ExpoCode").each do |document|
-    @documents << document.ExpoCode
-  end
 
-joined = "SELECT * FROM (cruises INNER JOIN spatial_groups " + 
-"ON spatial_groups.ExpoCode = cruises.ExpoCode) WHERE" 
-ind = "indian = ?"
-atl = "atlantic = ?"
-sou = "southern = ?"
-order_by = "ORDER BY area, Begin_Date ASC"
+    def indian 
+        documents = []
+        Document.find(:all, :select=>"ExpoCode").each do |document|
+            documents << document.ExpoCode
+        end
 
-  @indian_basin = SpatialGroups.find_by_sql(["#{joined} #{ind} AND #{atl} AND #{sou} #{order_by}",'1','0','0'])
-  @indian_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)} 
+        joined = "SELECT DISTINCT * FROM (cruises INNER JOIN spatial_groups " + 
+            "ON spatial_groups.ExpoCode = cruises.ExpoCode) WHERE" 
+        ind = "indian = ?"
+        atl = "atlantic = ?"
+        sou = "southern = ?"
+        order_by = "ORDER BY area, Begin_Date ASC"
 
-  @atlantic_basin = SpatialGroups.find_by_sql(["#{joined} #{ind} AND #{atl} #{order_by}",'1','1'])
-  @atlantic_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)} 
-  
-  @southern_basin = SpatialGroups.find_by_sql(["#{joined} #{ind} AND #{atl} AND #{sou} #{order_by}",'1','0','1'])
-  @southern_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)} 
+        @indian_basin = SpatialGroups.find_by_sql(
+            ["#{joined} #{ind} AND #{atl} AND #{sou} #{order_by}",'1','0','0'])
+        filter_sgroup_no_docs(documents, @indian_basin)
+        @indian_basin = group_spatial_groups(@indian_basin)
 
-  #@southern_basin = Cruise.find(:all, :conditions => ["`Group` LIKE ? AND (`Line` LIKE ?)", "%indian%", "S%"], :order=>"Line, Begin_Date ASC")
-  #@southern_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)}
+        @atlantic_basin = SpatialGroups.find_by_sql(
+            ["#{joined} #{ind} AND #{atl} #{order_by}",'1','1'])
+        filter_sgroup_no_docs(documents, @atlantic_basin)
+        @atlantic_basin = group_spatial_groups(@atlantic_basin)
 
-  #@indian_basin = Cruise.find(:all, :conditions => ["`Group` LIKE ? AND (`Line` LIKE ? OR `Line` LIKE ?)", "%indian%", "I%", "AIS%"], :order=>"Line, Begin_Date ASC")
-  #@indian_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)} 
+        @southern_basin = SpatialGroups.find_by_sql(
+            ["#{joined} #{ind} AND #{atl} AND #{sou} #{order_by}",'1','0','1'])
+        filter_sgroup_no_docs(documents, @southern_basin)
+        @southern_basin = group_spatial_groups(@southern_basin)
+    end
 
-  #@atlantic_basin = Cruise.find(:all, :conditions => ["`Group` LIKE ? AND (`Line` LIKE ? OR `Line` LIKE ? OR `Line` LIKE ? )", "%indian%", "A__", "AR%", "AJ%"], :order=>"Line, Begin_Date ASC")
-  #@atlantic_basin.delete_if {|cruise| !@documents.include?(cruise.ExpoCode)}
-end
+    private
+
+    def filter_sgroup_no_docs(documents, sgroups)
+        sgroups.delete_if {|cruise| !documents.include?(cruise.ExpoCode)} 
+    end
+
+    # Take a list of SpatialGroups and convert to a Hash with area name mapped
+    # to a list of SpatialGroups in the same order they were found.
+    def group_spatial_groups(sgs)
+        grouped = ActiveSupport::OrderedHash.new()
+        for sgroup in sgs
+            begin
+                if not grouped[sgroup.area].include?(sgroup)
+                    grouped[sgroup.area] << sgroup
+                end
+            rescue
+                grouped[sgroup.area] = [sgroup]
+            end
+        end
+        grouped
+    end
 end
